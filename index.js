a92404e373213e28daa73e1bad11da21820a25b9
pathum177 <pathummalsara177@gmail.com>
2025-05-28 18:54:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function downloadSessionData() {
         return false;
     }
     const sessdata = config.SESSION_ID.split("shadow~")[1];
-    const url = `https://pastebin.com/raw/${sessdata}`;
+    const url = `https://mega.nz/file/${sessdata}`;
     try {
         const response = await axios.get(url);
         const data = typeof response.data === 'string' ? response.data : JSON.stringify(response.data);
