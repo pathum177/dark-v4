eb5f73ecf0121b64fa4c90341b765bef2f41c988
pathum177 <pathummalsara177@gmail.com>
2025-05-28 22:11:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function downloadSessionData() {
         console.error('Please add your session to SESSION_ID env !!');
         return false;
     }
-    const sessdata = config.SESSION_ID.split("shadow~")[1];
+    const sessdata = config.SESSION_ID.split("PRABATH-MD~")[1];
     const url = `https://mega.nz/file/${sessdata}`;
     try {
         const response = await axios.get(url);
