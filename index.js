ca7723d2e473b3c96788ef2b142e650c997dfc3c
pathum177 <pathummalsara177@gmail.com>
2025-05-28 21:03:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -160,5 +160,7 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
